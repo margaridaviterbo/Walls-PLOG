be8164c6aebcbcb47d3def9e0c1b3633cb89b844
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2016-12-21 16:53:02 +0000

    Update aula.pl

--- Plog2/aula.pl
+++ Plog2/aula.pl
@@ -29,32 +29,42 @@ board1(15,[[14,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15],[14,B2,B3,B4,B5,
 board1(3,[[A,B,C],[D,E,F],[G,3,H],[I,J,K]]).
 
 
-
+%aplicar restrições horizontais a uma lista
 restrainHor(L,N):-automaton(L, _, L,[source(s),sink(i),sink(s)],[arc(s,1,s,[C+1]),arc(s,2,i),arc(i,1,i),arc(i,2,i)],[C],[0],[N],[]).
 
+%aplicar restrições verticais a uma lista
 restrainVer(L,N):-automaton(L, _, L,[source(s),sink(i),sink(s)],[arc(s,2,s,[C+2]),arc(s,1,i),arc(i,1,i),arc(i,2,i)],[C],[0],[N],[]).
 
+%mostrar o puzzle no ecrã
 display_board([L1|L2],Size):-display_line(L1),write(':'),nl,display_separator(Size,1),display_board(L2,Size).
 display_board([],_).
 
+%auxiliar para mostrar o puzzle no ecrã
 display_line([L1|L2]):-write(':'),(var(L1),write(' '),!;write(L1)),display_line(L2).
 display_line([]).
 
+%linha separadora (efeito visual)
 display_separator(Size,It):-It=<Size,!,write('..'),It1 is It+1,display_separator(Size,It1).
 display_separator(_,_):-write('.'),nl.
 
+%inicia os devidos campos e pede para se resolver o puzzle
 init(Size,L):-retract(board1(_,_)),asserta(board1(Size,L)),display_separator(Size,1),display_board(L,Size),nl,nl,main(L,Size),board1(_,OldPuzzle),display_separator(Size,1),draw_solution(OldPuzzle,L,Size).
 
+%resolve o puzzle
 main(Matrix,Size):-findNumberPaths(Matrix,1,Size,Paths,NumberArray),calculateDomain(Matrix,Domain),puzzle(Paths,NumberArray,Domain).
 
+%predicado para desenhar a solução do puzzle
 draw_solution([L1|L2],[L3|L4],Size):-draw_solution_line(L1,L3),write(':'),nl,display_separator(Size,1),draw_solution(L2,L4,Size).
 draw_solution([],[],_).
 
+%auxiliar para desenhar a solução do puzzle
 draw_solution_line([L1|L2],[L3|L4]):-write(':'),(var(L1),(L3==1,write('-'),!;write('|')),!;write(L1)),draw_solution_line(L2,L4).
 draw_solution_line([],[]).
 
+%declarar dominio, restrições e o labeling
 puzzle(Paths,NumberArray,Domain):-domain(Domain,1,2),restrain(Paths,NumberArray),labeling([],Domain).
 
+%todas as restriçoes relativas ao puzzle estão aqui e são chamadas por este predicado (apenas faz restrições)
 restrain([Path1,Path2,Path3,Path4|Paths],[N1|NR]):-
 length(Path1,TotalSize1),domain([VSize1],0,TotalSize1),restrainHor(Path1,VSize1),
 length(Path2,TotalSize2),domain([VSize2],0,TotalSize2),restrainHor(Path2,VSize2),
@@ -64,36 +74,47 @@ VSize31 #= VSize3/2, VSize41 #=VSize4/2,
 N1#=VSize1+VSize2+VSize41+VSize31,restrain(Paths,NR).
 restrain([],[]).
 
+%predicado que recebe um tabuleiro e retorna uma lista com as variáveis não declaradas sem repetições (= tabuleiro sem os números)
 calculateDomain([L1|L2],R):-calculateDomainAux(L1,R1),calculateDomain(L2,R2),append(R1,R2,R).
 calculateDomain([],[]).
 
+%predicado auxiliar de caulculateDomain
 calculateDomainAux([L1|L2],R):-number(L1),!,calculateDomainAux(L2,R).
 calculateDomainAux([L1|L2],[L1|R2]):-calculateDomainAux(L2,R2).
 calculateDomainAux([],[]).
 
+%retorna uma lista de listas com as variáveis não declaradas
 findNumberPaths(Matrix,X,Size,LR,NumberPath):-length(Matrix,Tamanho),Tamanho>=X,!,findNumberPathsAux(Matrix,[X,1],Size,L1,Number1),X1 is X+1,findNumberPaths(Matrix,X1,Size,L2,Number2),
 append(L1,L2,LR),append(Number1,Number2,NumberPath).
 findNumberPaths(_,_,_,[],[]).
 
+%para um dado número do puzzle encontra todas as variáveis que estão à sua esquerda, direita,cima e baixo e coloca numa lista de listas, sendo cada caminho uma lista
 findNumberPathsAux(Matrix,[X,Y],Size,[L1,L2,L3,L4|LR],[Value|Nr]):-Y=<Size,getElement(Matrix, X, Y, Value), number(Value),!,
 findHorizontalPath(Matrix,[X,Y],Size,L1,L2),length(Matrix,SizeVer),findVerticalPath(Matrix,[X,Y],SizeVer,L3,L4),
 Y1 is Y+1,findNumberPathsAux(Matrix,[X,Y1],Size,LR,Nr).
+
 findNumberPathsAux(Matrix,[X,Y],Size,LR,Nr):-Y=<Size,Y1 is Y+1,!,findNumberPathsAux(Matrix,[X,Y1],Size,LR,Nr).
 findNumberPathsAux(_,[_,_],_,[],[]).
 
+%pede os caminhos de um número para a esquerda e para a direita e devolve-os
 findHorizontalPath(Matrix,[X,Y],Size,L1,L2):-findRightPath(Matrix,[X,Y],Size,L1),findLeftPath(Matrix,[X,Y],Size,L2).
 
+%procura o caminho para a direita e devolve
 findRightPath(Matrix,[X,Y],Size,[Value|L2]):-Y1 is Y+1, Y1=<Size, getElement(Matrix, X, Y1, Value), \+ number(Value),!,findRightPath(Matrix, [X,Y1],Size,L2).
 findRightPath(_,[_,_],_,[]).
 
+%procura o caimnho para a esquerda e devolve
 findLeftPath(Matrix,[X,Y],_,[Value|L2]):-Y1 is Y-1, Y1>0, getElement(Matrix, X, Y1, Value), \+ number(Value),!,findLeftPath(Matrix, [X,Y1],_,L2).
 findLeftPath(_,[_,_],_,[]).
 
+%pede os caminhos de um número para cim e para baixo e devolve-os
 findVerticalPath(Matrix,[X,Y],Size,L1,L2):-findUpPath(Matrix,[X,Y],Size,L1),findLowPath(Matrix,[X,Y],Size,L2).
 
+%procura o caminho para baixo e devolve
 findLowPath(Matrix,[X,Y],Size,[Value|L2]):-X1 is X+1, X1=<Size, getElement(Matrix, X1, Y, Value), \+ number(Value),!,findLowPath(Matrix, [X1,Y],Size,L2).
 findLowPath(_,[_,_],_,[]).
 
+%procura o caminho para cima e devolve
 findUpPath(Matrix,[X,Y],_,[Value|L2]):-X1 is X-1, X1>0, getElement(Matrix, X1, Y, Value), \+ number(Value),!,findUpPath(Matrix, [X1,Y],_,L2).
 findUpPath(_,[_,_],_,[]).
 
@@ -101,7 +122,3 @@ findUpPath(_,[_,_],_,[]).
 getElement(Matrix, Row, Col, Value):-
   nth1(Row, Matrix, MatrixRow),
   nth1(Col, MatrixRow, Value).
-
-specialElement([L1|_],X,It,L1):-X==It,!.
-specialElement([],_,_,A):-!.
-specialElement([L1|L2],X,It,Ret):-It1 is It+1,specialElement(L2,X,I1,Ret).
